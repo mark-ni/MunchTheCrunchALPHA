b739082a10afda2ec12d61b0e0f1085d67426425
mark-ni <markni10423@gmail.com>
2019-11-02 16:42:52 -0400

    mapsactivity and mainactivity separate

--- app/src/main/java/com/example/munchthecrunchalpha/MainActivity.java
+++ app/src/main/java/com/example/munchthecrunchalpha/MainActivity.java
@@ -2,22 +2,29 @@ package com.example.munchthecrunchalpha;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+    private static final int ERROR_DIALOG_REQUEST = 9001;
+    
     protected EditText userLocationEditText;
     protected EditText foodLocationEditText;
     protected Button goButton;
-    protected TextView testing;
 
     protected String userLocation;
     protected String foodLocation;
-
+    //AIzaSyBEXHwGH2vJ2urprU3TDFxvF4O245kPi-U
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,8 +33,6 @@ public class MainActivity extends AppCompatActivity {
         userLocationEditText = findViewById(R.id.userLocationEditText);
         foodLocationEditText = findViewById(R.id.foodLocationEditText);
         goButton = findViewById(R.id.button);
-
-        testing = findViewById(R.id.testing);
 
         goButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,6 +45,29 @@ public class MainActivity extends AppCompatActivity {
     }
 
     protected void go(String theUserLocation, String theFoodLocation) {
-        testing.setText("" + userLocation + ", " + foodLocation);
+        Toast toast = Toast.makeText(getApplicationContext(), userLocation + ", " + foodLocation,Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    public boolean isServicesOK() {
+        Log.d(TAG, "isServicesOK: checking google services version");
+
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MainActivity.this);
+
+        if (available == ConnectionResult.SUCCESS) {
+            Log.d(TAG, "isServicesOK: Google Play Services is working");
+            return true;
+        }
+        else if (GoogleApiAvailability.getInstance().isUserResolvableError(available)) {
+
+            Log.d(TAG, "isServicesOK: an error occurred but we can fix it");
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MainActivity.this, available, ERROR_DIALOG_REQUEST);
+            dialog.show();
+        }
+        else {
+            Toast.makeText(this, "Can't make a map request", Toast.LENGTH_SHORT).show();
+        }
+        return false;
+
     }
 }
